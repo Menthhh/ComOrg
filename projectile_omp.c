4e58ac7e6e048e67671c2efd287de3afd817ae6f
menth <tonkla.p2004@gmail.com>
2023-11-08 12:08:52 +0700

    third-section

--- projectile_omp.c
+++ projectile_omp.c
@@ -1,76 +1,50 @@
 #include <stdio.h>
 #include <math.h>
 #include <omp.h>
-#define NUMBER_OF_THREADS 16
+
+// Shared variables
+double initial_velocity = 30.0; // m/s
+double angle = 45.0; // degrees
+double g = 9.81; // acceleration due to gravity (m/s^2)
+double height;
+double final_velocity;
 
 // Function to compute height
-double compute_height(double initial_velocity, double angle) {
+void compute_height() {
     double angle_rad = angle * M_PI / 180;
-    const double g = 9.81; // acceleration due to gravity
-    return initial_velocity * sin(angle_rad) * initial_velocity * sin(angle_rad) / (2 * g);
+    height = initial_velocity * sin(angle_rad) * initial_velocity * sin(angle_rad) / (2 * g);
 }
 
 // Function to compute final velocity
-double compute_final_velocity(double initial_velocity, double angle) {
+void compute_final_velocity() {
     double angle_rad = angle * M_PI / 180;
-    return initial_velocity * cos(angle_rad);
+    final_velocity = initial_velocity * cos(angle_rad);
 }
 
 int main() {
-    int num_samples = 10000000; // Number of samples
-    omp_set_num_threads(NUMBER_OF_THREADS);
+    // Set the number of threads for OpenMP
+    int num_threads = 16; // You can adjust this as needed
+    omp_set_num_threads(num_threads);
 
-    // Varying projectile variables
-    double initial_velocities[] = {30.0, 40.0, 50.0};
-    double angles[] = {30.0, 45.0, 60.0};
+    // Parallel region for computing height and final velocity
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        compute_height();
 
-    // Measure time without using threads
-    double start_time = omp_get_wtime();
-
-    for (int i = 0; i < num_samples; i++) {
-        // Compute height for various projectile variables
-        for (int iv = 0; iv < 3; iv++) {
-            for (int a = 0; a < 3; a++) {
-                double height = compute_height(initial_velocities[iv], angles[a]);
-                // Do something with the height, e.g., store or print it
-            }
-        }
-
-        // Compute final velocity for various projectile variables
-        for (int iv = 0; iv < 3; iv++) {
-            for (int a = 0; a < 3; a++) {
-                double final_velocity = compute_final_velocity(initial_velocities[iv], angles[a]);
-                // Do something with the final velocity, e.g., store or print it
-            }
-        }
+        #pragma omp section
+        compute_final_velocity();
     }
 
-    double stop_time = omp_get_wtime();
-
-    // Print the time taken without threads
-    printf("Without threads - Time taken: %.5f seconds\n", stop_time - start_time);
-
-    // Reset shared variables
-    start_time = omp_get_wtime();
-
-    // Measure time using OpenMP parallelization
-    #pragma omp parallel for
-    for (int i = 0; i < num_samples; i++) {
-        // Compute height and final velocity in parallel
-        for (int iv = 0; iv < 3; iv++) {
-            for (int a = 0; a < 3; a++) {
-                double height = compute_height(initial_velocities[iv], angles[a]);
-                double final_velocity = compute_final_velocity(initial_velocities[iv], angles[a]);
-                // Do something with the height and final velocity, e.g., store or print them
-            }
-        }
+    #pragma omp parallel
+    {
+        printf("Number of threads: %i\n", omp_get_num_threads());
     }
 
-    stop_time = omp_get_wtime();
 
-    // Print the time taken using OpenMP parallelization
-    printf("Using OpenMP - Time taken: %.5f seconds\n", stop_time - start_time);
-    printf("Max number of threads: %i\n", omp_get_max_threads());
+    printf("Max height: %.2f meters\n", height);
+    printf("Final velocity: %.2f m/s\n", final_velocity);
+    printf("max number of threads: %i\n", omp_get_max_threads());
 
     return 0;
 }
